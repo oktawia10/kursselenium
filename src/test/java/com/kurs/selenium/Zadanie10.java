d99fcc6f14a24fde5951d7adab9020d903894270
Oktawia Markowska <oktawia10@wp.pl>
2019-07-21 14:06:46 +0200

    poprawki 21.07

--- src/test/java/com/kurs/selenium/Zadanie10.java
+++ src/test/java/com/kurs/selenium/Zadanie10.java
@@ -48,11 +48,19 @@ public class Zadanie10 {
         System.out.println(": " + lastNameElement.getAttribute ("value"));
 
         WebElement element3 = driver.findElement(By.xpath("/html/body/div/div/form/div[3]/label"));
-        System.out.println(element3.getText());
+        System.out.print(element3.getText() +": ");
 
         List<WebElement> genders = driver.findElements(By.name("gender"));
         if (genders.get(1).isDisplayed()){
-        genders.get(1).click();} ; //w nawiasie który element 0 lub 2
+        genders.get(1).click();} //w nawiasie który element 0 lub 2
+
+        for (WebElement i : genders){
+            if (i.isSelected()){
+
+                WebElement genderLabel = i.findElement(By.xpath("./.."));
+                System.out.println(genderLabel.getText());
+            }
+        }
 
         WebElement element4 = driver.findElement(By.xpath("/html/body/div[1]/div/form/div[4]/label"));
         System.out.print(element4.getText());
@@ -100,6 +108,7 @@ public class Zadanie10 {
         companyElement.sendKeys("Testowa");
         System.out.println(": " + companyElement.getAttribute ("value"));
 
+
         WebElement element9 = driver.findElement(By.xpath("/html/body/div/div/form/div[9]/label"));
         System.out.print(element9.getText());
 
@@ -121,17 +130,26 @@ public class Zadanie10 {
         }
 
         WebElement element11 = driver.findElement(By.xpath("//html/body/div/div/form/div[11]/label"));
-        System.out.println(element11.getText());
+        System.out.print(element11.getText() + ": ");
 
         //checkbox
         List<WebElement> checkboxElements = driver.findElements(By.xpath("//div//label//input"));
-        checkboxElements.get(5).click();
+        if (checkboxElements.get(5).isDisplayed()) {
+            checkboxElements.get(5).click();
+        }
 
-        /* WebElement developmentWays= driver.findElement((By.cssSelector("div.col-sm-10 development-ways"));
-        List<WebElement> checkboxElements = developmentWays.findElements(By.xpath(".//input"));
-        checkboxElements.get(0).click();
+        WebElement developmentWays= driver.findElement((By.xpath("/html/body/div/div/form/div[11]/div")));
+        List<WebElement> checkbox2Elements = developmentWays.findElements(By.xpath(".//input"));
+        /*checkboxElements.get(0).click();
         checkboxElements.get(1).click();*/
 
+        for (WebElement j : checkbox2Elements){
+            if (j.isSelected()){
+
+                WebElement checkboxLabel = j.findElement(By.xpath("./.."));
+                System.out.println(checkboxLabel.getText());
+            }
+        }
 
         WebElement element12 = driver.findElement(By.xpath("//html/body/div/div/form/div[12]/label"));
         System.out.print(element12.getText());
